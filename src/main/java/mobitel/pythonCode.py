f0efeb300505e45833af56b59a30cad1b83dd964
Difna Jayashani <difnaj@mobitel.int>
2018-05-23 12:12:26 +0530

    idea file removed

--- src/main/java/mobitel/pythonCode.py
+++ src/main/java/mobitel/pythonCode.py
@@ -3,19 +3,32 @@
 
 from pip._vendor.distlib.compat import raw_input
 
-str = raw_input("Enter your input: ");
-print("Received input is : ", str)
+import MySQLdb
 
+#take input from the user
+startDate = raw_input("Enter the backup start date: ")
+print("Received input is : ", startDate)
+
+endDate = raw_input("Enter the backup end date: ")
+print("Received input is : ", endDate)
+
+
+
+
+#write to the file
 file_name="test.csv"
 try:
     file = open(file_name,"w+")
     try:
-        file.write( "Python is a great language.\nYeah its great!!\n")
+        file.write("Python is a great language.\nYeah its great!!\n")
     finally:
         print("Written content in the file successfully")
         file.close()
 except IOError:
     print("Unable to write to the file")
+
+
+
 
 
 
